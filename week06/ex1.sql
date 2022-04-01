612ed5cdc81ec83f1eee563bc3d4bcd47d0fa37e
Slauva <v.koshman@innopolis.university>
2022-04-01 16:26:59 +0300

    Full ex1

--- week06/ex1.sql
+++ week06/ex1.sql
@@ -32,3 +32,62 @@ SELECT DISTINCT c.sid
 FROM parts as p
          JOIN catalog c on p.pid = c.pid
 WHERE p.color = 'Green';
+
+--Task 5
+SELECT *
+FROM catalog as c
+WHERE NOT EXISTS(
+        (SELECT p.pid FROM parts as p)
+        EXCEPT
+        (SELECT cc.pid FROM catalog as cc WHERE c.sid = cc.sid)
+    );
+
+--Task 6
+SELECT *
+FROM catalog as c
+WHERE NOT EXISTS(
+        (SELECT p.pid FROM parts as p WHERE p.color = 'Red')
+        EXCEPT
+        (SELECT cc.pid FROM catalog as cc WHERE c.sid = cc.sid)
+    );
+
+--Task 7
+SELECT *
+FROM catalog as c
+WHERE NOT EXISTS(
+        (SELECT p.pid FROM parts as p WHERE p.color = 'Red' or p.color = 'Green')
+        EXCEPT
+        (SELECT cc.pid FROM catalog as cc WHERE c.sid = cc.sid)
+    );
+
+--Task 8
+SELECT *
+FROM catalog as c
+WHERE NOT EXISTS(
+        (SELECT p.pid FROM parts as p WHERE p.color = 'Red')
+        EXCEPT
+        (SELECT cc.pid FROM catalog as cc WHERE c.sid = cc.sid)
+    )
+UNION
+SELECT *
+FROM catalog as c
+WHERE NOT EXISTS(
+        (SELECT p.pid FROM parts as p WHERE p.color = 'Green')
+        EXCEPT
+        (SELECT cc.pid FROM catalog as cc WHERE c.sid = cc.sid)
+    );
+
+-- Task 9
+SELECT R.sid, G.sid
+FROM catalog as R
+         CROSS JOIN catalog as G
+WHERE R.pid = G.pid
+  AND R.sid != G.sid
+  AND R.cost > G.cost;
+
+-- Task 10
+SELECT R.pid
+FROM catalog as R
+         CROSS JOIN catalog as G
+WHERE R.pid = G.pid
+  AND R.sid != G.sid;
